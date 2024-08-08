7b49e4e1b04e9015bd7ca7958db0b7b09dd97399
CesarMarro <cemarroquin@ufm.edu>
2024-08-08 11:56:02 -0600

    Automating testing
    Added automated testing for counting sort alongside the algorithm itself

---
+++ counting_sort.py
@@ -0,0 +1,25 @@
+def counting_sort(arr):
+
+    k = max(arr) + 1
+
+    c = [0] * k
+
+    for i in range(len(arr)):
+        c[arr[i]] += 1
+
+    for i in range(1, k):
+        c[i] = c[i] + c[i - 1]
+
+    B = [None] * len(arr)
+
+    for i in range(len(arr) - 1, -1, -1):
+        c[arr[i]] -= 1
+        B[c[arr[i]]] = arr[i]
+
+    return B
+
+
+# Example usage:
+arr = [4, 2, 2, 8, 3, 3, 1]
+
+print("Sorted array:", counting_sort(arr))
